f4db43090c484852b626c187ffec5f8951a729c1
yzletter <yzletter@foxmail.com>
2025-12-06 13:39:01 +0800

    Channel 版本改用 Gin 框架

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # go-chatery
 
 ## 版本一：用 Channel 实现的聊天室
+
 ### 效果图
 ![效果图](/images/channel.png)
+
 ### 架构图
 以 User1 在群里发言为例
 ![架构图](/images/im版本一架构图.jpg)
+
+## 版本一：用 RabbitMQ 实现单聊、群聊
+
+### 架构图
+![架构图](/images/im版本二架构图.jpg)
--- im_channel/main.go
+++ im_channel/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,22 +18,24 @@ func main() {
 	hub := NewHub()
 	go hub.Run() // 启动 Hub
 
-	http.HandleFunc("/", loadHomePage) // 首页路由
-	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) { // 启动聊天
-		StartServer(hub, w, r)
+	engine := gin.Default()
+
+	engine.GET("/", loadHomePage) // 首页路由
+	engine.GET("/chat", func(ctx *gin.Context) { // 启动聊天
+		StartServer(hub, ctx.Writer, ctx.Request)
 	})
-	
+
 	addr := "localhost:" + *port // 拼接出监听地址
 	fmt.Printf("服务启动地址为 %s\n", addr)
 
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	if err := engine.Run(addr); err != nil {
 		fmt.Printf("服务启动失败 err : %s/n", err)
 	}
 }
 
 // 加载 html 页面
-func loadHomePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./im_channel/home.html")
+func loadHomePage(ctx *gin.Context) {
+	ctx.File("./im_channel/home.html")
 }
 
 // go run ./im_channel --port 5678
